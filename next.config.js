d8fcf698a55e388782c17ea2376e94182924bb17
jujumendezq <jujumendezq@gmail.com>
2026-09-11 18:13:21 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // If your GitHub Pages URL is https://USERNAME.github.io/pinoycare-ch/
+  // (i.e. you do NOT have a custom domain), uncomment the two lines below
+  // and replace 'pinoycare-ch' with your exact repo name:
+  //
+  // basePath: '/pinoycare-ch',
+  // assetPrefix: '/pinoycare-ch/',
+};
 
+module.exports = nextConfig;
