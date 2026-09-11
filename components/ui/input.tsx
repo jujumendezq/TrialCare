e39211fc8970d142a0ca7743de5fbb30c99d1914
jujumendezq <jujumendezq@gmail.com>
2026-09-11 18:20:22 +0200

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,1 +1,15 @@
+import * as React from 'react';
+import { cn } from '@/lib/utils';
 
+export const Input = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>(({ className, type, ...props }, ref) => (
+  <input
+    ref={ref}
+    type={type}
+    className={cn('input', className)}
+    {...props}
+  />
+));
+Input.displayName = 'Input';
